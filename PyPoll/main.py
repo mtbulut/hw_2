0c12b28ad358f5de9d2bdac6da37f33657a117ac
Memet Bulut <mbulut@tamusa.edu>
2019-11-22 17:12:10 -0600

    working on pypoll main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,32 @@ import os
 import csv
 
 with open(os.path.join("Resources","election_data.csv"), "r") as in_file:
-    for line in in_file:
-        # print(line)
+    in_csv = csv.reader(in_file)
+    header = next(in_csv)
+    print(header)
+
+
+
+
+
+    print('Election Results')
+
+    print("---------------------------")
+
+
+    "Total Votes: 3521001"
+    '-------------------------'
+    'Khan: 63.000 % (2218231)'
+    'Correy: 20.000 % (704200)'
+    'Li: 14.000 % (492940)'
+
+    'Tooley: 3.000% (105630)'
+    '-------------------------'
+    'Winner: Khan'
+    '-------------------------'
+
+
+
+
+
 
