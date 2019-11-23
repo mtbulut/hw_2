4096e22e97ecffe86a7b166497107ecb50d95ab9
Memet Bulut <mbulut@tamusa.edu>
2019-11-22 18:31:03 -0600

    first question is finished

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,18 +4,33 @@ import csv
 with open(os.path.join("Resources","election_data.csv"), "r") as in_file:
     in_csv = csv.reader(in_file)
     header = next(in_csv)
-    print(header)
 
+###################################################
+    count = 0
+    for row in in_csv:
+        voter = row[0]
+        count +=1
 
+    data = list(in_csv)
+    # print(data[0:5])
+    clean_data = [[int(e.strip()) for e in row] for row in data if row[2].strip() == 'khan']
+    print(clean_data)
 
 
 
     print('Election Results')
 
     print("---------------------------")
+    print("Total votes: " + str(count))
 
 
-    "Total Votes: 3521001"
+
+
+
+
+###############################################################################
+
+    "Total Votes: 3521001 DONE with THIS"
     '-------------------------'
     'Khan: 63.000 % (2218231)'
     'Correy: 20.000 % (704200)'
