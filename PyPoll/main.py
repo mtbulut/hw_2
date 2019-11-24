f0c367137a08285844bbe47270c84c04d48201d0
Memet Bulut <mbulut@tamusa.edu>
2019-11-23 23:09:40 -0600

    still working on pypoll main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,27 +1,48 @@
 import os
 import csv
+import pprint
+
 
 with open(os.path.join("Resources","election_data.csv"), "r") as in_file:
     in_csv = csv.reader(in_file)
     header = next(in_csv)
+    print(header)
 
 ###################################################
     count = 0
     for row in in_csv:
-        voter = row[0]
         count += 1
-
     data = list(in_csv)
 
-    for row in data:
-        if row[2] == "Khan":
-            for e in row:
-                print(e)
+####################################################
+
+    candidate_names = []
+
+    for row in in_csv:
+        if row[2] not in candidate_names:
+            candidate_names.append(row[2])
+        else:
+            continue
+    print(candidate_names)
+
+
+
+
+    # print(candidate_names)
+    print(candidate_names)
+
+    # for candidate in candidate_names:
+
+
+
 
 
     # clean_data = [[e for e in row] for row in data if row[2].strip() == 'Khan']
-    # print(clean_data)
-
+    # # print(clean_data)
+    #
+    # sorted_data = max(clean_data, key=lambda row: row[1])  # row[1] yerine ayni sekilde sum(row[2]) yazilabilir.
+    #
+    # pprint.pprint(sorted_data)
 
 
     print('Election Results')
