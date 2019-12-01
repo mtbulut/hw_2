06f8634fb7dfab0e77c095a2588191658cbd1495
Memet Bulut <mbulut@tamusa.edu>
2019-12-01 14:54:01 -0600

    Extra assigment has been added

--- ExtraContent/Instructions/PyBoss/mainB.py
+++ ExtraContent/Instructions/PyBoss/mainB.py
@@ -1,40 +1,109 @@
 
 import csv
 import numpy
+import datetime
 
+Emp_ID = []   # format NOT needed
+First_Name = []   # format needed
+Last_Name = []    # format needed
+DOB = []  # format needed
+SSN = []   # format needed
+State = []  # This is done 
+
+state_abbr = { 
+    'Alabama': 'AL',
+    'Alaska':'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    }
 
 # user input can be written here. 
 with open( "employee_data.csv", "r") as in_file: 
-    csv_reader = csv.DictReader(in_file)
-    with open("up_employee_data.csv", "w") as out_file:
-        fieldnames =["Emp ID","Name","DOB","SSN","State"]
-
-        csv_writer = csv.DictWriter(out_file, fieldnames=fieldnames)
-
-        csv_writer.writeheader()
-
-        for line in csv_reader:
-            # del line['State']
-            csv_writer.writerow(line)
-
-   
-
-   
-    # header1 = ['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State']
-    # data = list(my_csv)
-    # print(data) 
-
+    csv_reader = csv.reader(in_file)
     
-
-    # full_names = []
-    # for name in data:
-    #     full_name = name[1]
-    #     full_names.append(full_name)
-    #     for item in full_names:
-    #         first_last = item.strip().split(" ")
-    #         first_name = first_last[0]
-    #         last_name = first_last[1]
+    # skipping the hearder
+    header = next(csv_reader)   
+    data = list(csv_reader)
+      
+    for line in data:
             
+        Emp_ID.append(line[0]) 
+    # print(Emp_ID) 
+
+    full_names = []
+    for row in data:
+        full_name = row[1]
+        full_names.append(full_name)
+        for item in full_names:
+            first_last = item.strip().split(" ")
+            first_name = first_last[0]
+            last_name = first_last[1]
+        First_Name.append(first_name)
+        Last_Name.append(last_name)
+
+    # print(First_Name)
+    # print(Last_Name)
+    # orginizing the date time.
+        dob_Formatted = datetime.datetime.strptime(row[2], '%Y-%m-%d')
+        dob_Formatted = dob_Formatted.strftime('%m/%d/%Y') 
+
+        DOB.append(dob_Formatted)    
+    # print(DOB)
+        formated_ssn = list(row[3])
+        # for row in formated_ssn: 
+        formated_ssn[0:3]= ("*", "*", "*")
+        formated_ssn[4:6] = ("*", "*")
+        f_ssn = "".join(formated_ssn)
+
+        SSN.append(f_ssn)
+    # print(SSN)
+        row(4)
    
 
     
